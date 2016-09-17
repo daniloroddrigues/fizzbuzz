8b7eb1eeb1ece4d06538a3dd459e38a3f9d682fd
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2016-09-17 00:01:44 -0300

    Teste fizz buzz

---
+++ fizzbuzz.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+FizzBuzz
+
+1. Imprima Fizz se o numero for multiplo de 3
+2. Imprima Buzz se o numero for multiplo de 5
+3. Imprima FizzBuzz se o numero for multiplo de 3 e 5
+"""
+
+
+def fizz_buzz(param):
+    if param == 1 or param == 2:
+        return param
+    if param == 1:
+        return 1
+    if param == 2:
+        return 2
+    if param == 3:
+        return 'fizz'
+
+
+if __name__ == '__main__':
+    assert fizz_buzz(1) == 1
+    assert fizz_buzz(2) == 2
+    assert fizz_buzz(3) == 'fizz'
